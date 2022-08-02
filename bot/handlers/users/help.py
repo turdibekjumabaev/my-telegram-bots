a5887cd45b9b7334a19078766b1e40a1f86cc38d
Turdibek <turdibekjumabaev05@gmail.com>
2022-08-02 13:54:05 +0500

    new message

--- bot/handlers/users/help.py
+++ bot/handlers/users/help.py
@@ -6,8 +6,8 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Commands: ",
-            "/start - Bot started",
-            "/help - help")
+    text = ("<b>Túsinigim boyınsha siz bazı nárselerge túsinbedińiz.</b>",
+            "\nBottan paydalanıw tómendegishe ámelge asırıladı.",
+            "\n<i>Siz ózińiz  qallegen  sózdi botqa jiberesiz. Eger siz jibergen sóz tuwrı jazılģan bolsa, bot onıń tuwrı ekenligin kórsetedi. Keri jaģdayda, sizge tuwrı jazılģan sózler kórsetiledi.</i>")
 
     await message.answer("\n".join(text))
