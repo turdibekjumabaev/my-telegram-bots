b2feaa42151f28b1937479bef2cf2eadad79ac28
Turdibek <turdibekjumabaev05@gmail.com>
2022-08-02 13:07:17 +0500

    edited message

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -6,4 +6,4 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Sálem {message.from_user.full_name}, men sizge Qaraqalpaq tilindegi sózlerdi tuwrı jazılıwın tekserip alıwıńızģa járdem beremen.\n🤖 @kaaimla_bot")
+    await message.answer(f"Sálem {message.from_user.full_name}, men sizge Qaraqalpaq tilindegi sózlerdi tuwrı jazılıwın tekserip alıwıńızģa járdem beremen!\n🤖 @kaaimla_bot")
