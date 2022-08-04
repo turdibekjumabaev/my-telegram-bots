94135819fdd1b16d1e84c72ac840fe790c8634ba
Turdibek <turdibekjumabaev05@gmail.com>
2022-08-04 19:01:02 +0500

    add keyboard

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -2,8 +2,9 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from keyboards.inline.start_keyboard import startKeyboard
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Sálem, {message.from_user.full_name}!\nMeni Telegram gruppalarģa qosıwıńız múmkin.")
+    await message.answer(f"Sálem, {message.from_user.full_name}!\nMeni Telegram gruppalarģa qosıwıńız múmkin.", reply_markup=startKeyboard)
