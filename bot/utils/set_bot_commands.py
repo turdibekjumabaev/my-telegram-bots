3fa4f7fe5accbc9f171dd1c229fbd32af12360bc
Turdibek <turdibekjumabaev05@gmail.com>
2022-08-05 11:30:18 +0500

    completed

--- bot/utils/set_bot_commands.py
+++ bot/utils/set_bot_commands.py
@@ -4,7 +4,16 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "restart"),
-            types.BotCommand("help", "help me"),
+            types.BotCommand("start", "Bottı iske túsiriw"),
+            types.BotCommand("help", "Járdem"),
+            types.BotCommand("set_photo", "Gruppa súwretin ózgertiw"),
+            types.BotCommand("set_title", "Gruppa atın ózgertiw "),
+            types.BotCommand("set_description",
+                             "Gruppa haqqindaģı maģlumattı ózgertiw"),
+            types.BotCommand(
+                "ro", "Paydalanıwshını Read Only rejimge ótkiziw"),
+            types.BotCommand("unro", "Read Only rejimden shıģarıw"),
+            types.BotCommand("ban", "Ban"),
+            types.BotCommand("unban", "Bannan shiģarıw"),
         ]
     )
